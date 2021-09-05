ea6700939d8c428199137f407126cfe9db26eb16
codingstuff <codingstufftamil@gmail.com>
2021-09-05 19:57:34 +0530

    'initial'

---
+++ json.js
@@ -0,0 +1,51 @@
+let url = 'https://raw.githubusercontent.com/codingstufftamil/heatmap-camp/master/json/covid.json'
+let req = new XMLHttpRequest()
+
+let baseTemp
+let values =[]
+
+let xScale
+let yScale
+
+let xAxis
+let yAxis
+
+let width = 1200
+let height = 600
+let padding = 60
+
+let svg = d3.select('svg')
+
+let generateScales = () => {
+    
+
+}
+
+let drawCanvas = () => {
+    svg.attr('width', width)
+    svg.attr('height', height)
+}
+
+let drawCells = () => {
+
+    
+}
+
+let generateAxes = () => {
+
+    
+}
+
+req.open('GET', url, true)
+req.onload = () => {
+    let data = JSON.parse(req.responseText)
+    baseTemp = data.Object['Country']
+    // values = data.monthlyVariance
+    console.log(baseTemp)
+    console.log(values)
+    drawCanvas()
+    generateScales()
+    drawCells()
+    generateAxes()
+}
+req.send()
