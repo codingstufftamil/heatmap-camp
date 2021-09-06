3112a41182ed37bf5404b14a0c793860ce8877ec
codingstuff <codingstufftamil@gmail.com>
2021-09-06 17:04:23 +0530

    third

--- json.js
+++ json.js
@@ -18,6 +18,12 @@ let svg = d3.select('svg')
 
 let generateScales = () => {
     
+    xScale = d3.scaleLinear()
+                .range([padding, width - padding])
+
+
+    yScale = d3.scaleTime()
+                .range([padding, height - padding])
 
 }
 
@@ -28,20 +34,61 @@ let drawCanvas = () => {
 
 let drawCells = () => {
 
-    
+    svg.selectAll('rect')
+        .data(values)
+        .enter()
+        .append('rect')
+        .attr('class','cell')
+
+
+
+
+        svg.selectAll('rect')
+        .data(values)
+        .enter()
+        .append('rect')
+        .attr('class','cell')
+        .attr('fill', (item) => {
+            let variance = item['3/25/21']
+            if(variance <= 0){
+                return 'SteelBlue'
+            }else if(variance <= 5){
+                return 'LightSteelBlue'
+            }else if(variance <= 10){
+                return 'Orange'
+            }else{
+                return 'Crimson'
+            }
+        })
+
 }
 
 let generateAxes = () => {
 
+
+    let xAxis = d3.axisBottom(xScale)
+
+    svg.append('g')
+        .call(xAxis)
+        .attr('id','x-axis')
+        .attr('transform', 'translate(0, ' + (height-padding) + ')')
+
+
+
+    let yAxis = d3.axisLeft(yScale)
+
+	svg.append('g')
+        .call(yAxis)
+        .attr('id', 'y-axis')
+        .attr('transform', 'translate(' + padding + ', 0)')
     
 }
-
 req.open('GET', url, true)
 req.onload = () => {
     let data = JSON.parse(req.responseText)
-    baseTemp = data.Country/Region
-    // values = data.monthlyVariance
-    console.log(baseTemp)
+    // baseTemp = data.baseTemperature
+    values = data.monthlyVariance
+    // console.log(baseTemp)
     console.log(values)
     drawCanvas()
     generateScales()
